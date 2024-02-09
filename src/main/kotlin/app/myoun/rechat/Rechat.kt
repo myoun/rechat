b3e5d0cf6051b92ad11a97aac6e67e1648bf3795
myoun <aiden080605@gmail.com>
2024-02-09 18:27:22 +0900

    feat: write configurations and changed main file

---
+++ src/main/kotlin/app/myoun/rechat/Rechat.kt
@@ -0,0 +1,10 @@
+package app.myoun.rechat
+
+import net.fabricmc.api.ModInitializer
+
+class Rechat : ModInitializer {
+
+    override fun onInitialize() {
+        TODO("Not yet implemented")
+    }
+}
